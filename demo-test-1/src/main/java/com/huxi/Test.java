54b6cf748b5607e810900f64fd86553f54c1fe58
${HuXixxx} <${1302679574@qq.com}>
2023-01-07 23:51:39 +0800

    第7次修改

--- demo-test-1/src/main/java/com/huxi/Test.java
+++ demo-test-1/src/main/java/com/huxi/Test.java
@@ -6,6 +6,6 @@ package com.huxi;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println(6);
+        System.out.println(8);
     }
 }
